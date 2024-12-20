41f7682b6592712aea7757df1d8414c45700d887
ramya <nsriramya7@gmailcom>
2024-12-20 19:01:00 +0530

    done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import AuthRoutes from "./routes/AuthRoutes.js";
 import MessageRoutes from "./routes/MessageRoutes.js";
 import { Server } from "socket.io";
+const http  = require('http')
 
 dotenv.config();
 
@@ -17,9 +18,8 @@ app.use("/uploads/images/", express.static("uploads/images"));
 app.use("/api/auth", AuthRoutes);
 app.use("/api/messages", MessageRoutes);
 
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
-});
+const server = http.createServer(app)
+
 
 const io = new Server(server, {
     cors: {
