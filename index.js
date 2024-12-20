1b801cdff86d6df23a7289a34b52ed238d7aeac2
ramya <nsriramya7@gmailcom>
2024-12-20 17:10:38 +0530

    done

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { Server } from 'socket.io';
-const http  = require('http')
-import AuthRoutes from './routes/AuthRoutes.js';
-import MessageRoutes from './routes/MessageRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import AuthRoutes from "./routes/AuthRoutes.js";
+import MessageRoutes from "./routes/MessageRoutes.js";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use('/uploads/recordings/', express.static('uploads/recordings'));
-app.use('/uploads/images/', express.static('uploads/images'));
-app.use('/api/auth', AuthRoutes);
-app.use('/api/messages', MessageRoutes);
+app.use("/uploads/recordings/", express.static("uploads/recordings"));
+app.use("/uploads/images/", express.static("uploads/images"));
+app.use("/api/auth", AuthRoutes);
+app.use("/api/messages", MessageRoutes);
 
-const server = http.createServer(app); // Create an HTTP server
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+});
 
 const io = new Server(server, {
     cors: {
-        origin: 'https://tele-dailer-frontend.vercel.app', // Adjust as needed
-        credentials : true
+        origin: "https://tele-dailer-frontend.vercel.app",
     },
 });
 
 global.onlineUsers = new Map();
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
     global.chatSocket = socket;
 
-    socket.on('add-user', (userId) => {
+    socket.on("add-user", (userId) => {
         onlineUsers.set(userId, socket.id);
-        socket.broadcast.emit('online-users', {
+        socket.broadcast.emit("online-users", {
             onlineUsers: Array.from(onlineUsers.keys()),
         });
     });
 
-    socket.on('signout', (id) => {
+    socket.on("signout", (id) => {
         onlineUsers.delete(id);
-        socket.broadcast.emit('online-users', {
+        socket.broadcast.emit("online-users", {
             onlineUsers: Array.from(onlineUsers.keys()),
         });
     });
 
-    socket.on('send-msg', (data) => {
+    socket.on("send-msg", (data) => {
         const sendUserSocket = onlineUsers.get(data.to);
         if (sendUserSocket) {
-            socket.to(sendUserSocket).emit('msg-recieve', {
+            socket.to(sendUserSocket).emit("msg-recieve", {
                 from: data.from,
                 message: data.message,
             });
         }
     });
-
-    socket.on('outgoing-voice-call', (data) => {
+    socket.on("outgoing-voice-call", (data) => {
         const sendUserSocket = onlineUsers.get(data.to);
         if (sendUserSocket) {
-            socket.to(sendUserSocket).emit('incoming-voice-call', {
+            socket.to(sendUserSocket).emit("incoming-voice-call", {
                 from: data.from,
                 roomId: data.roomId,
                 callType: data.callType,
@@ -66,10 +66,10 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('outgoing-video-call', (data) => {
+    socket.on("outgoing-video-call", (data) => {
         const sendUserSocket = onlineUsers.get(data.to);
         if (sendUserSocket) {
-            socket.to(sendUserSocket).emit('incoming-video-call', {
+            socket.to(sendUserSocket).emit("incoming-video-call", {
                 from: data.from,
                 roomId: data.roomId,
                 callType: data.callType,
@@ -77,26 +77,22 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('reject-voice-call', (data) => {
+    socket.on("reject-voice-call", (data) => {
         const sendUserSocket = onlineUsers.get(data.from);
         if (sendUserSocket) {
-            socket.to(sendUserSocket).emit('voice-call-rejected');
+            socket.to(sendUserSocket).emit("voice-call-rejected");
         }
     });
 
-    socket.on('reject-video-call', (data) => {
+    socket.on("reject-video-call", (data) => {
         const sendUserSocket = onlineUsers.get(data.from);
         if (sendUserSocket) {
-            socket.to(sendUserSocket).emit('video-call-rejected');
+            socket.to(sendUserSocket).emit("video-call-rejected");
         }
     });
 
-    socket.on('accept-incoming-call', ({ id }) => {
+    socket.on("accept-incoming-call", ({ id }) => {
         const sendUserSocket = onlineUsers.get(id);
-        socket.to(sendUserSocket).emit('accept-call');
+        socket.to(sendUserSocket).emit("accept-call");
     });
 });
-
-export default (req, res) => {
-    app(req, res); // Handle request and response in serverless format
-};
