67a190cee127265fd1001302d5e4f6a8fd84e42b
ramya <nsriramya7@gmailcom>
2024-12-20 17:21:31 +0530

    done

--- index.js
+++ index.js
@@ -4,27 +4,40 @@ import cors from "cors";
 import AuthRoutes from "./routes/AuthRoutes.js";
 import MessageRoutes from "./routes/MessageRoutes.js";
 import { Server } from "socket.io";
+const http  = require('http')
 
 process.env.PORT=8080||process.env.PORT
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://tele-dailer-frontend.vercel.app",
+    credentials : true
+}))
 app.use(express.json());
+const PORT = process.env.PORT || 8080
+
+app.get('/',(request,response)=>{
+    response.json({
+        message : "Server running at " + PORT
+    })
+})
+
+
 
 app.use("/uploads/recordings/", express.static("uploads/recordings"));
 app.use("/uploads/images/", express.static("uploads/images"));
 app.use("/api/auth", AuthRoutes);
 app.use("/api/messages", MessageRoutes);
 
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
-});
+const server = http.createServer(app)
+
 
 const io = new Server(server, {
     cors: {
         origin: "https://tele-dailer-frontend.vercel.app",
+        credentials : true
     },
 });
 
